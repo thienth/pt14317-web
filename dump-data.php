74590d1388438209d167e1ada038c61df54c9d8d
thienth32 <thienth32@gmail.com>
2019-11-16 10:05:48 +0700

    using faker lib

--- dump-data.php
+++ dump-data.php
@@ -1,5 +1,17 @@
 <?php 
 require_once './commons/db.php';
+require_once './libs/Faker/autoload.php';
+
+$faker = Faker\Factory::create();
+
+// var_dump($faker->name('male'));
+// var_dump($faker->name('female'));
+// var_dump($faker->company);
+// var_dump($faker->realText);
+// var_dump($faker->imageUrl(640, 480));
+// var_dump($faker->image('public/images', 640, 480));
+
+// die;
 // tạo dump data cho bảng roles
 $roles = [
 	['name' => 'member', 'status' => 1],
@@ -7,10 +19,10 @@ $roles = [
 ];
 
 foreach ($roles as $key => $value) {
-	extract($value)
+	extract($value);
 	$sqlQuery = "insert into roles (name, status)
 				value ('$name', $status)";
-	execQuery($sqlQuery);
+	// execQuery($sqlQuery);
 }
 
 
@@ -41,8 +53,23 @@ foreach ($users as $key => $value) {
 	extract($value);
 	$sqlQuery = "insert into users (name, email, password, role_id)
 				value ('$name', '$email', '$password', '$role_id')";
-	execQuery($sqlQuery);
+	// execQuery($sqlQuery);
 }
 
+for ($i=0; $i < 20; $i++) { 
+	try{
+		$name = $faker->name;
+		$desc = $faker->realText;
+		$desc = str_replace("'", "\'", $desc);
+		$showMenu = rand(0, 1);
+		$sqlQuery = "insert into categories 
+					(name, description, show_menu)
+				values
+					('$name', '$desc', $showMenu)";
+		execQuery($sqlQuery);
+	}catch(Exception $ex){
+		var_dump($ex->getMessage());
+	}
+}
 
  ?>
